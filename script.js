99869328b1b633356de1aeeab37ef2a2de0c6c84
Sankeerth4026 <146646964+Sankeerth4026@users.noreply.github.com>
2024-03-24 23:51:52 +0530

    NetflixClone

---
+++ script.js
@@ -0,0 +1,8 @@
+function appear(){
+    const mail1 = document.getElementById('Name');
+    const nam =document.getElementById('naam');
+    const dis =document.getElementById('dissapear');
+    mail1.style.maxHeight = '85px';
+    nam.style.opacity ='100%';
+    dis.style.opacity='0';l
+}
